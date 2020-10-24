a58a818bb336b2f5c5b0d0810e7ddab329733d5a
Bapsk <baptiste.kief@gmail.com>
2020-10-23 20:15:11 -0400

    Add layout

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.where(user_id: current_user.id)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(article_params)
+    @article.user = current_user
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    @user = current_user
+    @posts = Post.all
   end
+
+  
 end
